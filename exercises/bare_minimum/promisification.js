e7567cff1494d1d6a0005d174b15a1758db74a49
lucyjunejack <gkvoid3@gmail.com>
2021-09-03 17:58:58 -0700

    Nodejs Promises

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -57,6 +57,8 @@ var readFileAndMakeItFunny = function(filePath, callback) {
   });
 };
 
+//var readFileAndMakeItFunnyAsync = Promise.callbackify(readFileAndMakeItFunny);
+
 var readFileAndMakeItFunnyAsync = filePath => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (err, file) => {
